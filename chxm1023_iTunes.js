e27d2fe2d1f5bcbc4ca2587707ea28b98dac46ef
Joanesnn <joannesn@outlook.com.au>
2025-03-13 18:06:45 +0800

    Update chxm1023_iTunes.js

--- chxm1023_iTunes.js
+++ chxm1023_iTunes.js
@@ -1,7 +1,6 @@
 /*************************************
-
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Joanesnn/Joanesnn-Quantumultx_chxm1023/main/chxm1023_iTunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
